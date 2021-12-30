0845762b7cd19fafc417b6de1634871fcc1a3cb6
Radmir Gurkov <ragux0@gmail.com>
2021-12-30 03:36:00 +0100

    Added state toogle, WIP: removing listeners, cleanup, previous player size storage

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 const button = document.getElementById("change-screen");
 
+const toggleButtonClassname = (isEnabled) => {
+  if (isEnabled) {
+    button.classList.add("btn--enabled");
+  } else {
+    button.classList.remove("btn--enabled");
+  }
+};
+
+// view init
+chrome.storage.sync.get("isEnabled", ({ isEnabled }) =>
+  toggleButtonClassname(isEnabled)
+);
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [key, { newValue }] of Object.entries(changes)) {
+    if (key === "isEnabled") {
+      toggleButtonClassname(newValue);
+    }
+  }
+});
+
+const toggleState = (isEnabled) => {
+  const newState = !isEnabled;
+  button.setAttribute("data-enabled", newState ? "true" : "false");
+  chrome.storage.sync.set({ isEnabled: newState });
+};
+
+const isEnabled = () => {
+  return button.getAttribute("data-enabled") == "true";
+};
+
 // When the button is clicked, inject setPageBackgroundColor into current page
 button.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  toggleState(isEnabled());
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: function () {
+      const previousWidth = 650;
+      const previousHeight = 370;
+
       const playerContainer = document.getElementById("yohoho");
       const yohohoIframe = document.getElementById("yohoho-iframe");
+
+      if (!playerContainer || !yohohoIframe) {
+        alert("Please select video first");
+        return;
+      }
+
       const resizePlayer = () => {
         playerContainer.style.top = 0;
         playerContainer.style.position = "fixed";
@@ -22,10 +63,25 @@ button.addEventListener("click", async () => {
         yohohoIframe.width = "100%";
         yohohoIframe.height = "100%";
       };
-      resizePlayer();
-      window.addEventListener("resize", () => {
+      const handleWindowResize = () => {
         requestAnimationFrame(resizePlayer);
-        isListenerAdded = true;
+      };
+
+      chrome.storage.sync.get("isEnabled", ({ isEnabled }) => {
+        if (isEnabled) {
+          resizePlayer();
+          window.addEventListener("resize", handleWindowResize);
+        } else {
+          console.log("removed");
+          window.removeEventListener("resize", handleWindowResize);
+          playerContainer.style.position = "relative";
+          playerContainer.style.width = `${previousWidth}px`;
+          playerContainer.style.height = `${previousHeight}px`;
+          yohohoIframe.style.width = `${previousWidth}px`;
+          yohohoIframe.style.height = `${previousHeight}px`;
+          yohohoIframe.width = `${previousWidth}px`;
+          yohohoIframe.height = `${previousHeight}px`;
+        }
       });
     },
   });
